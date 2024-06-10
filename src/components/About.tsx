761a072d7cae16e932337a257eabdbc505214c16
Yerassyl <serikyer0204@gmail.com>
2024-06-10 21:26:42 +0500

    Fixed About section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,6 +4,7 @@ const About = () => {
   return (
     <section className='p-8'>
         <h2 className="text-5xl font-bold mb-4">About Me</h2>
+        <p className='text-2xl font-bold'>Soon there will be more...</p>
     </section>
   );
 };
